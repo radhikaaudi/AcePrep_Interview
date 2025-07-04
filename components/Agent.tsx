24171cae110c41b26bb4f29e19b3c3ff370bca90
Radhika Audichya <radhikaaudichya15@gmail.com>
2025-07-04 18:14:05 +0530

    Implement interview generation

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,90 +1,67 @@
+import Image from "next/image";
+import {cn} from "@/lib/utils";
 
-import React from 'react'
-import Image from 'next/image'
-import { cn } from '@/lib/utils'
-
-enum CallStatus{
-  INACTIVE="INACTIVE",
-  CONNECTING="CONNECTING",
-  ACTIVE="ACTIVE",
-  FINISHED="FINISHED",
+enum CallStatus {
+    INACTIVE = 'INACTIVE',
+    CONNECTING = 'CONNECTING',
+    ACTIVE = 'ACTIVE',
+    FINISHED = 'FINISHED',
 }
 
-const Agent = ({userName}: AgentProps) => {
-  const callStatus = CallStatus.FINISHED;
-
+const Agent = ({ userName }: AgentProps) => {
+    const callStatus = CallStatus.FINISHED;
     const isSpeaking = true;
-    const messages=[
-      'Whats your  name?',
-      'My name is John Doe',
-      'What is your age?',
-      'I am 25 years old',
-      'What is your favorite color?',
-      'My favorite color is blue',
-      'What is your favorite food?',
-      'My favorite food is pizza',
+    const messages = [
+        'Whats your name?',
+        'My name is John Doe, nice to meet you!'
     ];
     const lastMessage = messages[messages.length - 1];
 
-
-  return (
-    <>
-      <div className="call-view">
-        <div className="card-interviewer">
-            <div className="avatar">
-                <Image src="/ai-avatar.png" alt="vapi" width={65} height={54} className="object-cover" />
-                {isSpeaking && <span className="animate-speak" />}
+    return (
+        <>
+        <div className="call-view">
+            <div className="card-interviewer">
+                <div className="avatar">
+                    <Image src="/ai-avatar.png" alt="vapi" width={65} height={54} className="object-cover" />
+                    {isSpeaking && <span className="animate-speak" />}
                 </div>
-                <h3>
-                    AI Inteview
-                    </h3>
+                <h3>AI Interviewer</h3>
             </div>
-            <div className ="card-content">
+
+            <div className="card-border">
                 <div className="card-content">
-                    <Image src="/user-avatar.png" alt ="user avatar" width={540} height={540} className="rounded-full object-cover size-[120px]" />
-
-                    <h3>{userName} </h3>
-                    </div>
-                    </div>  
-
-    </div>
-    {messages.length > 0 && (
-      <div className = "transcript-border">
-        <div className = "transcript">
-         <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0', 'animate-fadeIn opacity-100')}>
+                    <Image src="/user-avatar.png" alt="user avatar" width={540} height={540} className="rounded-full object-cover size-[120px]" />
+                    <h3>{userName}</h3>
+                </div>
+            </div>
+        </div>
+            {messages.length > 0 && (
+                <div className="transcript-border">
+                    <div className="transcript">
+                        <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0', 'animate-fadeIn opacity-100')}>
                             {lastMessage}
                         </p>
+                    </div>
+                </div>
+            )}
 
+            <div className="w-full flex justify-center">
+                {callStatus !== 'ACTIVE' ? (
+                    <button className="relative btn-call">
+                        <span className={cn('absolute animate-ping rounded-full opacity-75', callStatus !=='CONNECTING' & 'hidden')}
+                             />
 
-        </div>
-      </div>
-
-   ) }
-
-    <div className = "w-full flex justify-center">
-      {callStatus !== "ACTIVE" ?
-       (<button className= "relative btn-call">
-        <span className={cn('absolute animate-ping rounded-full opacity-75', callStatus !=='CONNECTING' & 'hidden')}
-        />
-         <span>
-         {callStatus === "INACTIVE" || callStatus === "FINISHED" ? "Call" : "..."} 
-        </span>
-
-      </button>)
-      : (
-        <button className = "btn-disconnect">
-          End
-
-        </button>
-      )
-      
-    }
-
-    </div>
-    
-    </>
-  
-  )
+                            <span>
+                                {callStatus === 'INACTIVE' || callStatus === 'FINISHED' ? 'Call' : '. . . '}
+                            </span>
+                    </button>
+                ) : (
+                    <button className="btn-disconnect">
+                        End
+                    </button>
+                )}
+            </div>
+        </>
+    )
 }
-
 export default Agent
